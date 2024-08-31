558ca3a1e86831ece59579a5583babd10de1a559
imshcheglov <Ivan_mix@mail.ru>
2024-08-31 23:43:56 +0500

    The third task is ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length === arr2.length) {
+		return arr1.every((value, index) => value === arr2[index]);
+	} else {
+		return false;
+	}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredUsers = users.filter(user => user.gender === gender);
+	if (filteredUsers.length === 0) {
+		return 0;
+	}
+
+	const ages = filteredUsers.map(user => user.age);
+	const averageAge = (ages.reduce((sum, age) => (sum + age), 0)) / filteredUsers.length;
+
+	return averageAge;
 }
